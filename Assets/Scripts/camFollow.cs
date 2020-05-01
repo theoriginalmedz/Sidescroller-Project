7ecdf5469598b8eade2c9f1da0294b18b11d372a
Medz <madmedz@hotmail.co.uk>
2020-05-01 04:16:25 +0100

    Camera Script
    Camera follows the player

---
+++ Assets/Scripts/camFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float offset;
+    private Transform playerTrans;
+
+    void Start()
+    {
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 tempPos = transform.position;
+
+        tempPos.x = playerTrans.position.x;
+        tempPos.y = playerTrans.position.y;
+
+        tempPos.x += offset;
+        tempPos.y += offset;
+
+
+        transform.position = tempPos;
+
+    }
+}
